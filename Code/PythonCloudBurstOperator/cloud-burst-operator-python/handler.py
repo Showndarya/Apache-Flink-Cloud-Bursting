7a19bb8121a51a7a227af745b70e107366ed64f4
Showndarya Madhavan <shmadhav@bu.edu>
2023-03-22 00:58:03 +0000

    Update python lambda function

--- Code/PythonCloudBurstOperator/cloud-burst-operator-python/handler.py
+++ Code/PythonCloudBurstOperator/cloud-burst-operator-python/handler.py
@@ -1,9 +1,12 @@
 import json
-
+import re
 
 def hello(event, context):
     event_body = json.loads(event['body'])
-    tokens = event_body["body"].split(" ")
+    values = re.split(r"\W", event_body["body"])
+    tokens = []
+    for v in values:
+        tokens.extend(re.split(r"\s+", v))
     response = {"statusCode": 200, "body": json.dumps(tokens)}
 
     return response
