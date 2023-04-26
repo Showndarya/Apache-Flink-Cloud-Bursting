118329ee628a0eba9865f8b2d8269af88cb58ec3
Showndarya <shmadhav@bu.edu>
2023-04-26 11:42:39 -0400

    Update metric logic

--- Code/deliverable-2/src/main/java/operator/ControllerProcessFunction.java
+++ Code/deliverable-2/src/main/java/operator/ControllerProcessFunction.java
@@ -8,8 +8,8 @@ import java.util.Random;
 
 public class ControllerProcessFunction extends ProcessAllWindowFunction<String, Tuple2<String, Boolean>, TimeWindow> {
 
-    private static final int MEASURE_INTERVAL_MS = 450;
-    private static final int INPUT_THRESHOLD = 2000;
+    private static final int MEASURE_INTERVAL_MS = 1000;
+    private static final int INPUT_THRESHOLD = 1000;
     private static final double CPU_THRESHOLD = 0.06;
 
     private long lastMeasureTime;
@@ -32,19 +32,20 @@ public class ControllerProcessFunction extends ProcessAllWindowFunction<String, 
             Double inputRate = MetricUtilities.getInputRate(job_id, getRuntimeContext().getTaskName());
             Long busyTimeMs = MetricUtilities.getBusyTime(job_id, getRuntimeContext().getTaskName());
 
-            Double busyTimeRatio = Math.max((double) (busyTimeMs / 1000), 0.8);
+            Double busyTimeRatio = (double) (busyTimeMs / 1000);
             System.out.println("busy time ratio" + busyTimeRatio);
 
             double offloading_ratio = rand.nextDouble() * inputRate;
             if (busyTimeRatio > CPU_THRESHOLD) {
                 shouldOffload = true;
             } else {
-                if (offloading_ratio < INPUT_THRESHOLD) {
+                if (offloading_ratio > INPUT_THRESHOLD) {
                     shouldOffload = true;
                 } else {
                     shouldOffload = false;
                 }
             }
+
             messageCount = 0;
             lastMeasureTime = currentTime;
         }
