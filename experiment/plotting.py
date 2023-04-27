007ff515f5bb7561402cbd22afc660a06b9b5a43
Yujie-Yan-S <cdyyj@bu.edu>
2023-04-27 17:18:16 -0400

    get experiment output

--- experiment/plotting.py
+++ experiment/plotting.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-arr=[1,2,3,4,5,6,7,8,9,10]
+arr=[1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500]
 data=[]
 for i in arr:
-    df = pd.read_csv('java latency '+str(i)+'.csv', header=None)
+    df = pd.read_csv('../java latency '+str(i)+'.csv', header=None)
     df=df.drop(df.columns[-1],axis=1)
     df = df.transpose()
     df['avg'] = df.mean(axis=1)
@@ -11,7 +11,7 @@ for i in arr:
     data.append(df['avg'])
 
 plt.boxplot(data)
-plt.xticks(range(1,11), arr)
+plt.xticks(range(1,24), arr)
 
 # set the title and labels
 plt.title('Box Plot for different batches java')
@@ -31,22 +31,23 @@ plt.ylabel('Latency (ms)')
 plt.show()
 
 
-df1 = pd.read_csv('java throughput.csv', header=None)
-df1=df1.drop(5,axis=1)
+df1 = pd.read_csv('../java throughput.csv', header=None)
+df1=df1.drop(df1.columns[-1],axis=1)
 df1['avg'] = df1.mean(axis=1)
 print(df1)
 # ax=df1['avg'].plot(x=range(1,11),y=df1['avg'],kind='line', legend=False)
-plt.plot(range(1,11),df1['avg'])
+plt.plot(range(1,24),df1['avg'])
 plt.title('Throughput vs batch')
 plt.ylabel('Throughput (Events / second)')
 plt.xlabel('Num of batches')
-plt.xticks([1,2,3,4,5,6,7,8,9,10])
+plt.xticks(range(1,24),[1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500])
+plt.figure(figsize=(15,8))
 
 
 plt.show()
 
 
-# arr=[1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100]
+# arr=[1,2,3,4,5,6,7,8,9,10,20,30,40]
 # data = []
 # for i in arr:
 #     df = pd.read_csv('../python latency '+str(i)+'.csv', header=None,na_values='')
@@ -58,7 +59,7 @@ plt.show()
 # # print(data)
 # plt.boxplot(data)
 # avgdata=[d.mean() for d in data]
-# plt.xticks(range(1,20), arr)
+# plt.xticks(range(1,14), arr)
 #
 # # set the title and labels
 # plt.title('Box Plot for different batches python')
